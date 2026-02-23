a2adc0ea95b4581251e2e06907ab8322b8d44a4e
Fran Domovic <93442516+frdomovic@users.noreply.github.com>
2026-02-23 14:53:09 +0100

    Revert "feat: add desktop login button (#103)" (#104)
    This reverts commit fd0ce54ad12c31a203124f96e7156e6649a4601f.

--- src/experimental/index.ts
+++ src/experimental/index.ts
@@ -1,6 +1,5 @@
 export { CalimeroProvider, useCalimero } from './CalimeroProvider';
 export { default as CalimeroConnectButton } from './CalimeroConnectButton';
-export { default as DesktopConnectButton } from './DesktopConnectButton';
 export { default as CalimeroLogo } from './CalimeroLogo';
 export { default as Toast } from './Toast';
 export * from './types';
